7339c9e615e9824e1587242d19a4ef87306b5efe
Cruz <cruzdiary@gmail.com>
2022-11-05 20:13:14 +0900

    Add RxCocoa dependency on HypeUI

--- Package.swift
+++ Package.swift
@@ -19,7 +19,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "HypeUI",
-            dependencies: ["RxSwift", "SnapKit"]),
+            dependencies: ["RxSwift", "RxCocoa", "SnapKit"]),
         .testTarget(
             name: "HypeUITests",
             dependencies: ["HypeUI"]),
